4fb784d321fa3f300d641df48387b4621d4f0404
1mposs1blyt <alexandr.bryaginya@gmail.com>
2022-01-10 14:13:03 +0700

    10.01.22 2 часа дня пора в школу =)

--- server.js
+++ server.js
@@ -27,72 +27,61 @@ nunjucks.configure(TmplPath, {
 // Роуты для сайта
 
 // app.get('',function(req, res){ something here }) <--- Гет запрос 
-
-
 app.get('/', function (request, response) {
+    let json = require(__dirname + '/json/TovarTypes.json');
 
-    // let json = require(__dirname + '/json/tovary.json');
-    // 
-    // for (const key in json) {
-    //     console.log(key, json[key]); // выводит ключи в объекте
-    // }
-    // console.log(dataToJson.TovarType)
-    response.render(__dirname + '/nunjucks/index.njk');
+    response.render(__dirname + '/nunjucks/index.njk', json);
 });
-app.get('/catalog', function (request, response) {
-    // let json = require(__dirname + '/json/tovary.json');
-
-    response.render(__dirname + "/nunjucks/catalog.njk");
-});
+// app.get('/catalog', function (request, response) {
+//     response.render(__dirname + "/nunjucks/catalog.njk");
+// });
 app.get('/profile', function (request, response) {
-    // let json = require(__dirname + '/json/tovary.json');
-
     response.render(__dirname + "/nunjucks/profile.njk");
 });
 app.get('/favorites', function (request, response) {
-    // let json = require(__dirname + '/json/tovary.json');
-
     response.render(__dirname + "/nunjucks/favorites.njk");
 });
 app.get('/cart', function (request, response) {
-    // let json = require(__dirname + '/json/tovary.json');
-
     response.render(__dirname + "/nunjucks/cart.njk");
 });
-app.get('/CPU', function (request, response) {
-    let json = require(__dirname + '/json/tovarCPU.json');
-    // let json2 = JSON.stringify(json)
-    response.render(__dirname + "/nunjucks/TovarsPage.njk", json)
-    // let PgN = request.query.PgN;
-    // if (PgN == "1" || PgN == "2" || PgN == "3" || PgN == "4" || PgN == "5") {
-    //     response.sendFile(__dirname + "/nunjucks/CPU.njk/?${pagefNum}")
-    // }    
-});
+// app.get('/processors', function (request, response) {
+//     let json = require(__dirname + '/json/tovarCPU.json');
+//     response.render(__dirname + "/nunjucks/TovarsPage.njk", json);
+// });
 
-app.get('/CPU/cpu/', function (request, response) {
-    let json = require(__dirname + '/json/tovarCPU.json');
-    response.render(__dirname + "/nunjucks/TovarPage.njk", json);
-});
-app.get('/motherboards', function (request, response) {
-    let json = require(__dirname + '/json/tovarMotherboard.json');
+// app.get('/goods/processors/processor', function (request, response) {
+//     let json = require(__dirname + '/json/tovarCPU.json');
+//     response.render(__dirname + "/nunjucks/TovarPage.njk", json);
+// });
+// app.get('/goods/motherboards', function (request, response) {
+//     let json = require(__dirname + '/json/tovarMotherboard.json');
+//     response.render(__dirname + "/nunjucks/TovarsPage.njk", json);
+// });
+// app.get('/goods/motherboards/motherboard/', function (request, response) {
+//     let json = require(__dirname + '/json/tovarMotherboard.json');
+//     response.render(__dirname + "/nunjucks/TovarPage.njk", json);
+// });
+// app.get('/goods/cases', function (request, response) {
+//     let json = require(__dirname + '/json/tovarCase.json');
+//     response.render(__dirname + "/nunjucks/TovarsPage.njk", json);
+// });
+// app.get('/goods/cases/case/', function (request, response) {
+//     let json = require(__dirname + '/json/tovarCase.json');
+//     response.render(__dirname + "/nunjucks/TovarPage.njk", json);
+// });
+app.get('/catalog/*/', function (request, response) {
+    let json = require(__dirname + '/json/TovarTypes.json');
     response.render(__dirname + "/nunjucks/TovarsPage.njk", json);
 });
-app.get('/motherboards/motherboard/', function (request, response) {
-    let json = require(__dirname + '/json/tovarMotherboard.json');
-    response.render(__dirname + "/nunjucks/TovarPage.njk", json);
+app.get('/product/*/*', function (request, response) {
+    response.render(__dirname + "/nunjucks/TovarPage.njk");
 });
-
-
-app.get('/cases', function (request, response) {
-    let json = require(__dirname + '/json/tovarCase.json');
-    response.render(__dirname + "/nunjucks/TovarsPage.njk", json);
-});
-app.get('/cases/case/', function (request, response) {
-    let json = require(__dirname + '/json/tovarCase.json');
-    response.render(__dirname + "/nunjucks/TovarPage.njk", json);
-});
-
-
+// ниже как НАДО, выше как есть =)
+// app.get('/product/*/{{TovarName}}', function (request, response) {
+//     response.render(__dirname + "/nunjucks/TovarPage.njk");
+// });
+// /product/1235fas/i3-10100f
+// /catalog/catalogNumber/processors
 // Роуты конец
 app.listen(port, function () {
     console.log(`Server stated on: http://${hostname}:${port}`)
